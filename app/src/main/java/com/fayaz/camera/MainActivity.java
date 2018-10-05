f94cc132477b069f11d9095e5bd171a79f1200dc
fayaz <fayazfz07@gmail.com>
2018-10-05 23:29:22 +0530

    Initial commit

---
+++ app/src/main/java/com/fayaz/camera/MainActivity.java
@@ -0,0 +1,92 @@
+package com.fayaz.camera;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private Button openCamera;
+    private ImageView image;
+
+    private static final int CAMERA_REQUEST_CODE = 100;
+
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        openCamera = (Button) findViewById(R.id.btnOpenCamera);
+        image = (ImageView) findViewById(R.id.ivImage);
+
+        mProgress = new ProgressDialog(this);
+
+        openCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+
+            mProgress.setMessage(("Uploading Image..."));
+            mProgress.show();
+
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            image.setImageBitmap(bitmap);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] byteData = baos.toByteArray();
+
+            UploadTask uploadTask = mStorage.child("Photos").child(data.getData().getLastPathSegment()).putBytes(byteData);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    mProgress.dismiss();
+                    Toast.makeText(MainActivity.this, "Upload failed " + exception , Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    mProgress.dismiss();
+                    Toast.makeText(MainActivity.this, "Upload success", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+}
